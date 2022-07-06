a66132835f9935396fde36856ae2f6d3eb4fa1e9
Viktoriya Ovcharenko <vickymmm2012@gmail.com>
2022-07-06 17:09:20 +0200

    №2

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
-const refs = {
-  openMenuBtn: document.querySelector('[data-menu-open]'),
-  closeMenuBtn: document.querySelector('[data-menu-close]'),
-  menu: document.querySelector('[data-menu]'),
-  body: document.querySelector('body'),
-};
-refs.openMenuBtn.addEventListener('click', toggleMenu);
-refs.closeMenuBtn.addEventListener('click', toggleMenu);
-function toggleMenu() {
-  refs.menu.classList.toggle('is-hidden');
-  refs.body.classList.toggle('no-scroll');
-}
-})();
+    const refs = {
+      openModalBtn: document.querySelector("[data-menu-open]"),
+      closeModalBtn: document.querySelector("[data-menu-close]"),
+      modal: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
